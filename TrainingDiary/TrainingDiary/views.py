ad4a9eed49857b7d3845419380454f97b6a20310
Leo Tavaila <leo.tavaila@morssoftware.com>
2022-01-21 17:32:56 +0200

    db connection works

--- TrainingDiary/TrainingDiary/views.py
+++ TrainingDiary/TrainingDiary/views.py
@@ -3,8 +3,13 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 from TrainingDiary import app
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:admin@localhost:5432/TrainingDiary"
+db = SQLAlchemy(app)
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +40,9 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/maxlifts')
+def maxlifts():
+    result = db.session.execute("SELECT * FROM maxlifts")
+    data = result.fetchall()
+    return render_template("maxlifts.html", data = data[0][0])
